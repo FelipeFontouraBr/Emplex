02569e01e7db1907edca7adfedaf5aab3a2e9be2
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-08-01 17:34:36 +0200

    Chagend variable to state

--- scripts/create-service.js
+++ scripts/create-service.js
@@ -1,5 +1,14 @@
-document
+
+function populateState() {
+    const stateSelect = document.querySelector("select[name=state]")
+
+
+}
+
+
+
+/*document
     .querySelector("select[name=city]")
     .addEventListener("change", function() {
-        
-    })
+        console.log('Opa, mudei véi')
+    })*/
