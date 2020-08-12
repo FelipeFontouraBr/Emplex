bb78fa137927e1010e6f2722cdd3f7cea9575170
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-08-12 21:24:45 +0200

    Remove class hide

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 const buttonSearch = document.querySelectorAll("#page-home main a")
 
+const modal = document.querySelector("#modal")
+
 buttonSearch.addEventListener("click", () => {
-    
+    modal.classList.remove("hide")
 })
