563c3711bba880070069d041777caaa271bc14c0
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-08-12 21:37:57 +0200

    Added modal.classlita add "hide"

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
-const buttonSearch = document.querySelectorAll("#page-home main a") // Selecionando o botão
+const buttonSearch = document.querySelector("#page-home main a") // Selecionando o botão
 const modal = document.querySelector("#modal") // Selecionando o modal
 const close = document.querySelector("#modal .header a") // Selecionando a imagem close
 
 buttonSearch.addEventListener("click", () => {
     modal.classList.remove("hide")
+})
+
+close.addEventListener("click", () => {
+    modal.classList.add("hide")
 })
