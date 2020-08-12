23f1e3622611b21d10358b96a790c2c21ab4913d
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-08-12 21:29:00 +0200

    Fixed error: get #modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const buttonSearch = document.querySelectorAll("#page-home main a") // Selecionando o botão
 const modal = document.querySelector("#modal") // Selecionando o modal
-const close = document.querySelector("#img") // Selecionando a imagem close
+const close = document.querySelector("modal .header a") // Selecionando a imagem close
 
 buttonSearch.addEventListener("click", () => {
     modal.classList.remove("hide")
