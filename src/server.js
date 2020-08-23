d485b9b7e0079928005883fada2957664d10fde6
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-08-23 12:42:18 +0200

    Setting static file: create-service

--- src/server.js
+++ src/server.js
@@ -11,4 +11,9 @@ server.get("/", function(req,res) {
     res.sendFile(__dirname + "/views/index.html")
 })
 
+// Create-service
+server.get("/", function(req,res) {
+    res.sendFile(__dirname + "/views/create-service.html")
+})
+
 server.listen(3000) // Start the server
