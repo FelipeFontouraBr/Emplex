1af132fddb66a71ce2f3f4506779e921adfe6bb0
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-08-25 22:44:16 +0200

    sendFile for .render

--- src/server.js
+++ src/server.js
@@ -14,12 +14,12 @@ nunjucks.configure("src/views", {
 // Setting ways of the application
 // Home
 server.get("/", function(req,res) {
-    res.sendFile(__dirname + "/views/index.html")
+    res.render(__dirname + "/views/index.html")
 })
 
 // Create-service
 server.get("/create-service", function(req,res) {
-    res.sendFile(__dirname + "/views/create-service.html")
+    res.render(__dirname + "/views/create-service.html")
 })
 
 server.listen(3000) // Start the server
