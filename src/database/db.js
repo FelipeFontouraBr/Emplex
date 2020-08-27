4de347791f63973cef047d9ecec6152e15dee0b3
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-08-27 16:17:19 +0200

    Create database sqlite

--- src/database/db.js
+++ src/database/db.js
@@ -2,4 +2,4 @@
 const sqlite3 = require("sqlite3").verbose()
 
 // Create an object of the database
-const db =  new sqlite3.Database("./src/database/database.db")
+const db =  new sqlite3.Database("./src/database/database.db") //Iniciando um novo objeto ao constructor
